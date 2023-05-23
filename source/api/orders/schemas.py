dbeb2e0f40c18d7879a7d4af5dac114c17c1c42f
sometastycake <16300181+sometastycake@users.noreply.github.com>
2023-05-24 00:09:51 +0300

    Update CreateOrderRequest

--- source/api/orders/schemas.py
+++ source/api/orders/schemas.py
@@ -8,16 +8,25 @@ from source.api.orders.enums import OrderSide
 
 class CreateOrderRequest(pydantic.BaseModel):
     symbol: str = Field(description='Торговая пара')
-    volume: Decimal = Field(description='Объем в долларах')
-    number: int = Field(description='На сколько ордеров нужно разбить этот объем')
-    amountDif: Decimal = Field(description=(
-        'Разброс в долларах, в пределах которого случайным образом '
-        'выбирается объем в верхнюю и нижнюю сторону'
-    ))
+    volume: Decimal = Field(description='Объем в долларах', gt=0)
+    number: int = Field(description='На сколько ордеров нужно разбить этот объем', gt=0)
+    amountDif: Decimal = Field(
+        description=(
+            'Разброс в долларах, в пределах которого случайным образом '
+            'выбирается объем в верхнюю и нижнюю сторону'
+        ),
+        gt=0,
+    )
     side: OrderSide = Field(description='Сторона торговли (SELL или BUY)')
-    priceMin: Decimal = Field(description=(
-        'Нижний диапазон цены, в пределах которого нужно случайным образом выбрать цену'
-    ))
-    priceMax: Decimal = Field(description=(
-        'Верхний диапазон цены, в пределах которого нужно случайным образом выбрать цену'
-    ))
+    priceMin: Decimal = Field(
+        description=(
+            'Нижний диапазон цены, в пределах которого нужно случайным образом выбрать цену'
+        ),
+        ge=0,
+    )
+    priceMax: Decimal = Field(
+        description=(
+            'Верхний диапазон цены, в пределах которого нужно случайным образом выбрать цену'
+        ),
+        ge=0,
+    )
