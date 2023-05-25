126a105c7f269dbb8153d5e7343ff6218476e876
sometastycake <16300181+sometastycake@users.noreply.github.com>
2023-05-25 14:42:10 +0300

    Add logging

--- source/api/orders/api.py
+++ source/api/orders/api.py
@@ -4,12 +4,14 @@ from starlette.responses import JSONResponse
 from source.api.orders.handler import create_order_handler
 from source.api.orders.schemas import CreateOrderRequest, CreateOrderResponse
 from source.clients.binance.errors import BinanceHttpError
+from source.logger import logger
 
 orders_router = APIRouter(prefix='/order')
 
 
 @orders_router.post(path='/create', response_model=CreateOrderResponse)
 async def create_order(request: CreateOrderRequest):
+    logger.info(f'Request {request}')
     if request.priceMin > request.priceMax:
         response = CreateOrderResponse(
             success=False,
--- source/api/orders/handler.py
+++ source/api/orders/handler.py
@@ -3,6 +3,7 @@ from source.clients.binance.client import BinanceClient
 from source.clients.binance.schemas.market.errors import NotFoundSymbolInExchangeInfo
 from source.clients.binance.schemas.market.schemas import ExchangeInfoResponse
 from source.enums import OrderType, SymbolStatus
+from source.logger import logger
 
 
 async def create_order_handler(request: CreateOrderRequest) -> CreateOrderResponse:
@@ -11,21 +12,25 @@ async def create_order_handler(request: CreateOrderRequest) -> CreateOrderRespon
         try:
             symbol = exchange_info.get_symbol(request.symbol.upper())
         except NotFoundSymbolInExchangeInfo:
+            logger.warning(f'Not found symbol in exchange info symbol={request.symbol}')
             return CreateOrderResponse(
                 success=False,
                 error='Not found trading symbol',
             )
         if not symbol.isSpotTradingAllowed:
+            logger.warning(f'Spot trading disabled symobl={request.symbol}')
             return CreateOrderResponse(
                 success=False,
                 error='Spot trading disabled',
             )
         if symbol.status in (SymbolStatus.HALT, SymbolStatus.BREAK, SymbolStatus.AUCTION_MATCH):
+            logger.warning(f'Wrong trading symbol status symbol={request.symbol}')
             return CreateOrderResponse(
                 success=False,
                 error='Wrong trading symbol status',
             )
         if OrderType.LIMIT not in symbol.orderTypes:
+            logger.warning(f'Limit order disabled symbol={request.symbol}')
             return CreateOrderResponse(
                 success=False,
                 error='Limit order disabled',
