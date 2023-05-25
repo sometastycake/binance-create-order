cb459c89edf7d20cac03fe2618d5fbda32c7b926
sometastycake <16300181+sometastycake@users.noreply.github.com>
2023-05-25 03:58:11 +0300

    Set default value for BINANCE_API_TIMEOUT

--- source/config.py
+++ source/config.py
@@ -7,7 +7,7 @@ class Config(pydantic.BaseSettings):
     BINANCE_API_KEY: str
     BINANCE_SECRET_KEY: str
     BINANCE_API_URL: str
-    BINANCE_API_TIMEOUT: int
+    BINANCE_API_TIMEOUT: int = 15
 
     def get_binance_api(self, path: str) -> str:
         return str(URL(self.BINANCE_API_URL).with_path(path))
